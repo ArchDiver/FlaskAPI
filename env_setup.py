e51a56b35be6d8937a8adee0cf9e06e67e7beff0
LSC <lelandscrawford@gmail.com>
2020-12-14 22:25:41 -0500

    adding env_setup

--- env_setup.py
+++ env_setup.py
@@ -3,3 +3,6 @@ import string
 import random
 
 def add_to_env(length):
+    with open('.env', 'w+') as file:
+        database_url= 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user='postgres',pw='LittleZoeNugget3689',url='127.0.0.1:5433',db='api_test_db')
+        
