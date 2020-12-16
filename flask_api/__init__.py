d33c5caddfe74edd950ecdc71700250db25a48f8
LSC <lelandscrawford@gmail.com>
2020-12-15 22:00:24 -0500

    __init__

---
+++ flask_api/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from flask_marshmellow import Marshmellow
+
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+mirgrate = Migrate(app, db)
+ma = Marshmellow(app)
+
+login_manager = LoginManager(app)
+
+from flask_api import models, routes
