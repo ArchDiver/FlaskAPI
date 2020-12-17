fd0925ac8513558046e7542eabb864e725d5391b
LSC <lelandscrawford@gmail.com>
2020-12-16 21:41:08 -0500

    config

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config():
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'random_acts_of_kindness!!36'
+    SQLACHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLACHEMY_TRACK_MODIFICATIONS = False # silence the depreation warning
