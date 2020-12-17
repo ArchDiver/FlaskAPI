bb454ac14e43bf0725d9dee5db9150d2ded4ab93
LSC <lelandscrawford@gmail.com>
2020-12-16 20:24:49 -0500

    forms

--- flask_api/forms.py
+++ flask_api/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, Email
+
+class UserForm(FlaskForm):
+    name = StringField('Name', validators = [DataRequired()])
+    email = StringField('Email', validators = [DataRequired(), Email])
+    password = StringField('Password', validators = [DataRequired()])
+    confirm_password = StringField('Confirm Password', validators = [DataRequired(), EqualTo('Password')])
+    
