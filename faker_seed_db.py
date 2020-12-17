5bbf62aecc32d43c9573d5cd9181369af1bc405a
LSC <lelandscrawford@gmail.com>
2020-12-16 21:51:45 -0500

    config

---
+++ faker_seed_db.py
@@ -0,0 +1,20 @@
+from faker import faker
+
+def getProfile():
+    fake = Faker()
+    return faker.profile()
+
+from flask_api.models import Patient
+from flask_api import db
+import os
+def seedData():
+    form seed_num in range(10):
+    data = getProfile()
+    print(os.environ.get('DATABASE_URL'))
+    print(data['name'], data['blood_group'])
+    patient = Patient(
+        data['name'],\
+        data['sex'],\
+        data['address'],\
+        data['ssn'],\
+            data['mail'])
