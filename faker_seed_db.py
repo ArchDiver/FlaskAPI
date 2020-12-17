b3f9e90171ac0d7b4847e7784513572d63020019
LSC <lelandscrawford@gmail.com>
2020-12-16 22:41:20 -0500

    Finished for Dec 15-still getting errors in faker and models

--- faker_seed_db.py
+++ faker_seed_db.py
@@ -1,24 +1,26 @@
-from faker import faker
+from faker import Faker
 
 def getProfile():
     fake = Faker()
-    return faker.profile()
+    return fake.profile()
 
+import os
 from flask_api.models import Patient
 from flask_api import db
-import os
+
 def seedData():
     for seed_num in range(10):
         data = getProfile()
         print(os.environ.get('DATABASE_URL'))
-        print(data['name'], data['blood_group'])
+        print(data['name'],data['blood_group'])
         patient = Patient(
             data['name'],\
             data['sex'],\
             data['address'],\
             data['ssn'],\
+            data['blood_group'],\
             data['mail'])
-        db.session.add(patient)
-        db.session.commit()
-seedData()
 
+        db.session.add(patient)
+        db.session.commit()        
+seedData()
