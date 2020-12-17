85bca53dc58abb9881a063c448ba43e69ab98d77
LSC <lelandscrawford@gmail.com>
2020-12-16 22:12:44 -0500

    faker

--- faker_seed_db.py
+++ faker_seed_db.py
@@ -8,17 +8,17 @@ from flask_api.models import Patient
 from flask_api import db
 import os
 def seedData():
-    form seed_num in range(10):
-    data = getProfile()
-    print(os.environ.get('DATABASE_URL'))
-    print(data['name'], data['blood_group'])
-    patient = Patient(
-        data['name'],\
-        data['sex'],\
-        data['address'],\
-        data['ssn'],\
-        data['mail'])
-    db.session.add(patient)
-    db.session.commit()
+    for seed_num in range(10):
+        data = getProfile()
+        print(os.environ.get('DATABASE_URL'))
+        print(data['name'], data['blood_group'])
+        patient = Patient(
+            data['name'],\
+            data['sex'],\
+            data['address'],\
+            data['ssn'],\
+            data['mail'])
+        db.session.add(patient)
+        db.session.commit()
 seedData()
 
