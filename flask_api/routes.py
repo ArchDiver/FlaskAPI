b888ec8ad1230a6a09193483fe376cafa095de65
LSC <lelandscrawford@gmail.com>
2020-12-26 22:07:54 -0500

    routing

--- flask_api/routes.py
+++ flask_api/routes.py
@@ -115,16 +115,16 @@ def register():
 #   # route to the LOGIN page
 @app.route('/users/login', methods = ['GET', 'POST'])
 def login():
-    # form = LoginForm()
-    # email = form.email.data
-    # password = form.password.data
-    # logged_user = User.query.filter(User.email == email).first()
-    # if logged_user and check_password_hash(logged_user.password, password):
-    #     login_user(logged_user)
-    #     flash(f"You are now logged in as {logged_user}.")
-    #     return redirect(url_for('get_key'))
-    # flash(f"Not a valid user or password. Please try again or register")
-    # return render_template('login.html', form = form)
+    form = LoginForm()
+    email = form.email.data
+    password = form.password.data
+    logged_user = User.query.filter(User.email == email).first()
+    if logged_user and check_password_hash(logged_user.password, password):
+        login_user(logged_user)
+        flash(f"You are now logged in as {logged_user}.")
+        return redirect(url_for('get_key'))
+    flash(f"Not a valid user or password. Please try again or register")
+    return render_template('login.html', form = form)
 
 @app.route('/users/logout')
 def logout():
