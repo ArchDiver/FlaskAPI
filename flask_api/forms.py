1abca85754a1cc93703d1e659b14556cba59293a
LSC <lelandscrawford@gmail.com>
2020-12-28 18:57:42 -0500

    forms

--- flask_api/forms.py
+++ flask_api/forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, EqualTo, Email
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+# from wtforms.validators import DataRequired, EqualTo, Email
 
 class UserForm(FlaskForm):
     name = StringField('Name', validators = [DataRequired()])
