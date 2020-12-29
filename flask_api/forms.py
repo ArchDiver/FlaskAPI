810d076173593d6cd1f0a330f8b6eeddb20bad84
LSC <lelandscrawford@gmail.com>
2020-12-29 17:58:40 -0500

    forms

--- flask_api/forms.py
+++ flask_api/forms.py
@@ -13,3 +13,6 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired()])
     submit = SubmitField()
+
+class PatientForm(FlaskForm):
+    
