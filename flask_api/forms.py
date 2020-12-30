f05e407f76503b0579d20302d2397feecd1592d4
LSC <lelandscrawford@gmail.com>
2020-12-29 19:05:31 -0500

    forms

--- flask_api/forms.py
+++ flask_api/forms.py
@@ -1,18 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, EqualTo, Email
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired, EqualTo, Email, Length
 
 class UserForm(FlaskForm):
     name = StringField('Name', validators = [DataRequired()])
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('Password', message="Passwords must match")])
-    submit = SubmitField()
+    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password', message="Passwords must match")])
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired()])
-    submit = SubmitField()
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
 class PatientForm(FlaskForm):
     firstName = request.json['first_name']
